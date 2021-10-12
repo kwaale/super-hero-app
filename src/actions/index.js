c6d9985b681f0db9358ae749a96fa1141f70072b
Knut Waale <kwaale@github.com>
2021-10-12 20:22:02 -0300

    implementando actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,22 @@
+import axios from "axios";
+
 export const PRIMERA_ACTION = "PRIMERA_ACTION";
+export const LOGIN = "LOGIN";
+
+export function primeraAccion(){
+    return{
+        type:PRIMERA_ACTION,
+        payload:console.log('Imprime payload')
+    }
+}
+export function login(payload){
+    return async (dispatch)=>{
+        try {
+            const {data} = await axios.post('http://challenge-react.alkemy.org/');
+            data
+        } catch (error) {
+            
+        }
+        
+    }
+}
